7f23749f6e4bed58614e0faeb1a922ae2975bc7d
patida12 <lpham3023@gmail.com>
2020-10-28 23:38:02 +0700

    edit tab setting class

--- src/router/layouts/SettingsClass.vue
+++ src/router/layouts/SettingsClass.vue
@@ -1,71 +1,70 @@
 <template>
-  <div>
-    <side-bar class="sidebar"> </side-bar>
-    <div class="block">
-      <v-form @submit.prevent="changeClassInfo">
-        <h2> Class infomation </h2>
-        <v-avatar size="69">
-          <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
-        </v-avatar>
-        <v-btn @click="changeAvatar"> Change avatar </v-btn>
-        <v-row>
-          <v-col cols="1" sm="6">
-            <h5>Class name</h5>
-            <v-text-field v-model="classname" outlined> </v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="1" sm="6">
-            <h5> Class code </h5>
-            <v-text-field v-model="classcode" outlined> </v-text-field>
-          </v-col>
-        </v-row>
-        <v-btn type="submit" color="primary"> Save </v-btn>
-      </v-form>
-      <v-divider class="divider"></v-divider>
-      <v-form>
-        <h2>Class settings</h2>
-        <v-checkbox
-          v-model="requiredToJoin"
-          :label="`Require approval to join`"
-        ></v-checkbox>
-        <h5>Participant messaging</h5>
-        <v-radio-group v-model="participantMess">
-          <v-radio label="On" value="On"> </v-radio>
-          <v-radio label="Role-based" value="Rolebased"> </v-radio>
-          <v-radio label="Off" value="Off"> </v-radio>
-        </v-radio-group>
-      </v-form>
-      <v-divider class="divider"></v-divider>
-      <v-form>
-        <h2>Class owners</h2>
-      </v-form>
-      <v-divider class="divider"></v-divider>
-      <div>
-        <h3>Archiving</h3>
-        <v-row>
-          <v-col sm="3" cols="12">
-            <h5>Reuse class code</h5>
-            <v-btn color="error" @click="reuseClassCode"
-              >Reuse class code</v-btn
-            >
-          </v-col>
-          <v-col sm="3" cols="12">
-            <h5>Remove class</h5>
-            <v-btn color="error" @click="removeClass">Remove class</v-btn>
-          </v-col>
-        </v-row>
-      </div>
-    </div>
-  </div>
+  <classline>
+    <v-container style="margin-left: 260px; margin-top: 100px;">
+      <v-card style="padding: 15px;">
+        <v-form @submit.prevent="changeClassInfo">
+          <h2> Class infomation </h2>
+          <v-avatar size="69">
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+          </v-avatar>
+          <v-btn @click="changeAvatar"> Change avatar </v-btn>
+          <v-row>
+            <v-col cols="1" sm="6">
+              <h5>Class name</h5>
+              <v-text-field v-model="classname" outlined> </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="1" sm="6">
+              <h5> Class code </h5>
+              <v-text-field v-model="classcode" outlined> </v-text-field>
+            </v-col>
+          </v-row>
+          <v-btn type="submit" color="primary"> Save </v-btn>
+        </v-form>
+        <v-divider class="divider"></v-divider>
+        <v-form>
+          <h2>Class settings</h2>
+          <v-checkbox
+            v-model="requiredToJoin"
+            :label="`Require approval to join`"
+          ></v-checkbox>
+          <h5>Participant messaging</h5>
+          <v-radio-group v-model="participantMess">
+            <v-radio label="On" value="On"> </v-radio>
+            <v-radio label="Role-based" value="Rolebased"> </v-radio>
+            <v-radio label="Off" value="Off"> </v-radio>
+          </v-radio-group>
+        </v-form>
+        <v-divider class="divider"></v-divider>
+        <v-form>
+          <h2>Class owners</h2>
+        </v-form>
+        <v-divider class="divider"></v-divider>
+        <div>
+          <h3>Archiving</h3>
+          <v-row>
+            <v-col sm="3" cols="12">
+              <h5>Reuse class code</h5>
+              <v-btn color="error" @click="reuseClassCode"
+                >Reuse class code</v-btn
+              >
+            </v-col>
+            <v-col sm="3" cols="12">
+              <h5>Remove class</h5>
+              <v-btn color="error" @click="removeClass">Remove class</v-btn>
+            </v-col>
+          </v-row>
+        </div>
+      </v-card>
+    </v-container>
+  </classline>
 </template>
 <script>
-import SideBar from "../../components/SideBar.vue";
+import Classline from "../layouts/Classline.vue";
 export default {
   name: "SettingsClass",
-  components: {
-    SideBar,
-  },
+  components: { Classline },
   data() {
     return {
       classname: "Math",
@@ -91,12 +90,6 @@ export default {
 };
 </script>
 <style scoped>
-.block {
-  width: 100%;
-  height: auto;
-  margin-left: 255px;
-  padding: 30px;
-}
 .divider {
   margin-top: 20px;
   margin-bottom: 20px;
