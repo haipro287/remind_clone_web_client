40d55510c71d4d969c8f83640a97e60609c58ba5
patida12 <lpham3023@gmail.com>
2020-11-01 01:24:44 +0700

    fix ui tab setting class

--- src/router/views/Files.vue
+++ src/router/views/Files.vue
@@ -1,34 +1,37 @@
 <template>
   <v-container class="mx-0 ml-5">
-    <v-list max-width="500px" subheader two-line>
-      <v-subheader>Month</v-subheader>
+    <v-card class="pa-4" elevation="1">
+      <v-card-title>Files</v-card-title>
+      <v-list max-width="500px" subheader two-line>
+        <v-subheader>Month</v-subheader>
 
-      <v-list-item
-        v-for="file in files"
-        :key="file.name"
-        @click.stop="showFileDetails = true"
-      >
-        <v-list-item-avatar>
-          <v-icon class="blue lighten-1" dark>
-            mdi-file
-          </v-icon>
-        </v-list-item-avatar>
+        <v-list-item
+          v-for="file in files"
+          :key="file.name"
+          @click.stop="showFileDetails = true"
+        >
+          <v-list-item-avatar>
+            <v-icon class="blue lighten-1" dark>
+              mdi-file
+            </v-icon>
+          </v-list-item-avatar>
 
-        <v-list-item-content>
-          <v-list-item-title v-text="file.name"></v-list-item-title>
+          <v-list-item-content>
+            <v-list-item-title v-text="file.name"></v-list-item-title>
 
-          <v-list-item-subtitle v-text="file.time"></v-list-item-subtitle>
-        </v-list-item-content>
+            <v-list-item-subtitle v-text="file.time"></v-list-item-subtitle>
+          </v-list-item-content>
 
-        <file-details v-model="showFileDetails"></file-details>
+          <file-details v-model="showFileDetails"></file-details>
 
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="blue lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-    </v-list>
+          <v-list-item-action>
+            <v-btn icon>
+              <v-icon color="blue lighten-1">mdi-information</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-card>
   </v-container>
 </template>
 
