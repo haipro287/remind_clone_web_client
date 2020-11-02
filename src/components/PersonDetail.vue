f60bfe1bcb5642bacf29cd7bb41f3f4042fae8b6
patida12 <lpham3023@gmail.com>
2020-11-02 10:42:36 +0700

    add dialog person detail

--- src/components/PersonDetail.vue
+++ src/components/PersonDetail.vue
@@ -1,35 +1,6 @@
 <template>
   <v-row>
-    <v-dialog v-on="on" v-model="dialog" persistent max-width="600px">
-      <v-card>
-        <v-toolbar dark color="primary" style="height: 80px;">
-          <v-toolbar-title
-            style="margin-left: 200px; margin-top: 20px; font-size: 25px"
-            >Join a class</v-toolbar-title
-          >
-          <v-spacer></v-spacer>
-        </v-toolbar>
-        <v-divider></v-divider>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col>
-                <v-text-field prefix="@" label="Class code"></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">
-            Close
-          </v-btn>
-          <v-btn color="blue darken-1" text @click="dialog = false">
-            Save
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <v-dialog v-on="on" v-model="dialog"> </v-dialog>
   </v-row>
 </template>
 <script>
@@ -42,17 +13,4 @@ export default {
   methods: {},
 };
 </script>
-<style scoped>
-.joinClass {
-  color: #2196f3;
-  width: 200px;
-  padding-left: 18px;
-  margin-left: 12px;
-}
-
-.iconAdd {
-  color: #2196f3;
-  margin-right: 7px;
-  margin-bottom: 2px;
-}
-</style>
+<style scoped></style>
